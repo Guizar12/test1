c1297fd07d6b6a2859d763a918276a151743c987
Guillermo Guizar <guillermoguizar@Guizar-MacBook.local>
2019-07-03 14:48:57 -0500

    Upload

---
+++ Test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Guillermo Guizar on 7/2/19.
+//  Copyright © 2019 Scotiabank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
